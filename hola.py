309ab6b652624af1d19cd675c7b0f60b1b6837b6
GabrielGuzAng <gabriel.guz.an@gmail.com>
2020-05-10 17:56:02 -0300

    first commit

---
+++ hola.py
@@ -0,0 +1,6 @@
+print("\n Primeras operaciones \n")
+
+num1=int(input(" \n ingrese el primer número: "))
+num2=int(input(" \n ingrese el segundo número: "))
+
+print("\n\n", num1+num2, "\n\n", num1-num2)
