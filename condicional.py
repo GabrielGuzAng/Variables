da5186f9c7ceea70c1f14dfd7439e2600e2e4af0
GabrielGuzAng <gabriel.guz.an@gmail.com>
2020-05-12 09:03:47 -0300

    condicionales

---
+++ condicional.py
@@ -0,0 +1,10 @@
+num1=float(input("\n Ingrese el primer número\n"))
+num2=float(input("\n Ingrese el segundo número\n"))
+num3=float(input("\n Ingrese el tercer número\n"))
+
+prom=(num1+num3+num2)/3
+
+if(prom>25):
+	print("\n Pasa con %.2f\n"%prom)
+else:
+	print("\n No pasa, tiene promedio de %.2f\n"%prom)
