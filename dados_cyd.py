574cd677b538c87afab86033f806ede3ca13520b
GabrielGuzAng <gabriel.guz.an@gmail.com>
2020-05-15 15:25:17 -0300

    funcion de calabozos y dragones

---
+++ dados_cyd.py
@@ -0,0 +1,100 @@
+
+import random
+
+dado1= random.randint(1,6)
+dado2= random.randint(1,6)
+dado3= random.randint(1,6)
+dado4= random.randint(1,6)
+
+#print("\n los dados dicen: dado1=%i dado2=%i dado3=%i dado4=%i \n"%(dado1,dado2,dado3,dado4))
+
+menor=min(dado4,dado3,dado2,dado1)
+suma=dado1+dado2+dado3+dado4
+Fuerza=suma-menor
+
+print("\n Fuerza=",Fuerza,"\n\n")
+
+#######################################################
+
+
+dado1= random.randint(1,6)
+dado2= random.randint(1,6)
+dado3= random.randint(1,6)
+dado4= random.randint(1,6)
+
+#print("\n los dados dicen: dado1=%i dado2=%i dado3=%i dado4=%i \n"%(dado1,dado2,dado3,dado4))
+
+menor=min(dado4,dado3,dado2,dado1)
+suma=dado1+dado2+dado3+dado4
+Carisma=suma-menor
+
+print( "Carisma=",Carisma,"\n\n")
+
+################################################################
+
+
+dado1= random.randint(1,6)
+dado2= random.randint(1,6)
+dado3= random.randint(1,6)
+dado4= random.randint(1,6)
+
+#print("\n los dados dicen: dado1=%i dado2=%i dado3=%i dado4=%i \n"%(dado1,dado2,dado3,dado4))
+
+menor=min(dado4,dado3,dado2,dado1)
+suma=dado1+dado2+dado3+dado4
+Destreza=suma-menor
+
+
+print( "Destreza=",Destreza,"\n\n")
+
+################################################
+
+
+dado1= random.randint(1,6)
+dado2= random.randint(1,6)
+dado3= random.randint(1,6)
+dado4= random.randint(1,6)
+
+#print("\n los dados dicen: dado1=%i dado2=%i dado3=%i dado4=%i \n"%(dado1,dado2,dado3,dado4))
+
+menor=min(dado4,dado3,dado2,dado1)
+suma=dado1+dado2+dado3+dado4
+
+Inteligencia=suma-menor
+
+print( "Inteligencia=",Inteligencia,"\n\n")
+
+################################################
+
+dado1= random.randint(1,6)
+dado2= random.randint(1,6)
+dado3= random.randint(1,6)
+dado4= random.randint(1,6)
+
+#print("\n los dados dicen: dado1=%i dado2=%i dado3=%i dado4=%i \n"%(dado1,dado2,dado3,dado4))
+
+menor=min(dado4,dado3,dado2,dado1)
+suma=dado1+dado2+dado3+dado4
+
+Constitucion=suma-menor
+
+print(" Constitucion=",Constitucion,"\n\n")
+
+################################################
+
+dado1= random.randint(1,6)
+dado2= random.randint(1,6)
+dado3= random.randint(1,6)
+dado4= random.randint(1,6)
+
+#print("\n los dados dicen: dado1=%i dado2=%i dado3=%i dado4=%i \n"%(dado1,dado2,dado3,dado4))
+
+menor=min(dado4,dado3,dado2,dado1)
+suma=dado1+dado2+dado3+dado4
+
+Sabiduria=suma-menor
+
+print( "Sabiduria=",Sabiduria,"\n\n")
+
+################################################
+
