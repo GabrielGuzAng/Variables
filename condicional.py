d58ab6099468207467883459029ae851aaf303c5
GabrielGuzAng <gabriel.guz.an@gmail.com>
2020-05-14 22:57:01 -0300

    practica while

--- condicional.py
+++ condicional.py
@@ -1,11 +1,18 @@
-num1=float(input("\n Ingrese el primer número\n"))
-num2=float(input("\n Ingrese el segundo número\n"))
-num3=float(input("\n Ingrese el tercer número\n"))
 
-prom=(num1+num3+num2)/3
-
-if(prom>25):
-	print("\n Pasa con %.2f\n"%prom)
+num=float(input("\n Ingrese número para adherir al promedio. O ingrese 0 para salir\n"))
+cant=0
+prom=0
+while(num!=0):
+	if((num>0) and (num<=10)):
+		cant=cant+1
+		prom=prom+num
+	else:
+		print("\n El número ingresado se encuentra fuera del rango aceptable\n")
+	num=float(input("\n Ingrese número para adherir al promedio. O ingrese 0 para salir\n"))
+	
+prom_final=prom/cant
+if(prom>=6):
+	print("\n Pasa con %.2f y cant %i \n"%(prom_final,cant))
 else:
-	print("\n No pasa, tiene promedio de %.2f\n"%prom)
+	print("\n No pasa, tiene promedio de %.2f y cant \n"%(prom_final,cant))
 print("\n finn del progrAMA\n")
