8aee696a670923f3975a685c345a9a0a929d8f6d
GabrielGuzAng <gabriel.guz.an@gmail.com>
2020-05-24 14:20:28 -0300

    funciones básicas de archivos

---
+++ clase1_archivos.py
@@ -0,0 +1,13 @@
+
+datos="hola a todos 5"
+#file=open("datos.txt","w")
+#file.write(datos)
+#file.close()
+
+with open("datos.txt","w") as file:
+	file.write(datos)
+
+with open("datos.txt","r") as file:
+	text=file.read()
+
+print(text)
